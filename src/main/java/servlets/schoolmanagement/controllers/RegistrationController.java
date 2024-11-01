bdcd36b26de35cba7fe837fdc46edf5f9f335a06
Melvin <103605665+melvinhqb@users.noreply.github.com>
2024-11-01 02:19:14 +0100

    Merge pull request #1
    * Clean de RegistrationController

--- src/main/java/servlets/schoolmanagement/controllers/RegistrationController.java
+++ src/main/java/servlets/schoolmanagement/controllers/RegistrationController.java
@@ -1,5 +1,6 @@
 package servlets.schoolmanagement.controllers;
 
+import servlets.schoolmanagement.dto.UserRegistrationDto;
 import servlets.schoolmanagement.models.Enumerations.Departement;
 import servlets.schoolmanagement.models.entity.RegistrationRequest;
 import servlets.schoolmanagement.models.entity.Student;
@@ -15,7 +16,6 @@ import servlets.schoolmanagement.services.TeacherService;
 import java.sql.Date;
 
 @Controller
-@RequestMapping("/api")
 public class RegistrationController {
 
     private final StudentService studentService;
@@ -29,75 +29,69 @@ public class RegistrationController {
         this.requestService = requestService;
     }
 
-    @GetMapping("/")
-    public String home(Model model) {
-        return "index"; // Renvoie à la page d'accueil
+    @GetMapping
+    public String home() {
+        return "redirect:/auth";
+    }
+
+    @GetMapping("/auth")
+    public String showAuthPage() {
+        return "index";
     }
 
     @PostMapping("/register")
-    public String register(
-            @RequestParam String userType,
-            @RequestParam String email,
-            @RequestParam String lastname,
-            @RequestParam String firstname,
-            @RequestParam(required = false) String date_of_birth,
-            @RequestParam String password,
-            @RequestParam(required = false) String department,
-            RedirectAttributes redirectAttributes) {
+    public String register(@ModelAttribute UserRegistrationDto userDto, RedirectAttributes redirectAttributes) {
 
         // Vérification de l'existence de l'utilisateur
-        if (studentService.userExists(email, lastname, firstname)) {
+        if (studentService.userExists(userDto.getEmail(), userDto.getLastname(), userDto.getFirstname())) {
             redirectAttributes.addFlashAttribute("errorMessage", "Un utilisateur avec ces informations existe déjà.");
-            return "redirect:/api/"; // Redirige vers la page d'accueil avec un message d'erreur
+            return "redirect:/"; // Redirige vers la page d'accueil avec un message d'erreur
         }
 
         try {
-            if ("student".equalsIgnoreCase(userType)) {
+            if ("student".equalsIgnoreCase(userDto.getUserType())) {
                 // Conversion de la date de naissance
-                Date dateOfBirth = Date.valueOf(date_of_birth);
+                Date dateOfBirth = Date.valueOf(userDto.getDateOfBirth());
 
                 //Creation of a new student
-                Student student = new Student(firstname, lastname, dateOfBirth, email, password);
-
-
-                // isVerified = 0
+                Student student = new Student(
+                        userDto.getFirstname(),
+                        userDto.getLastname(),
+                        dateOfBirth,
+                        userDto.getEmail(),
+                        userDto.getPassword()
+                );
                 studentService.registerStudent(student);
 
-
                 RegistrationRequest request = new RegistrationRequest(student);// Crée une demande
-
-
-
                 requestService.saveRequest(request);
+
                 redirectAttributes.addFlashAttribute("message", "Demande d'inscription d'étudiant soumise avec succès !");
 
-            } else if ("teacher".equalsIgnoreCase(userType)) {
-
-                Teacher teacher= new Teacher(firstname, lastname, email, password,Departement.fromDisplayName(department) );
-
-
-                // isVerified = 0
+            } else if ("teacher".equalsIgnoreCase(userDto.getUserType())) {
+                Teacher teacher= new Teacher(
+                        userDto.getFirstname(),
+                        userDto.getLastname(),
+                        userDto.getEmail(),
+                        userDto.getPassword(),
+                        Departement.fromDisplayName(userDto.getDepartment())
+                );
                 teacherService.registerTeacher(teacher);
 
-
                 RegistrationRequest request = new RegistrationRequest(teacher);// Crée une demande
-
-
-
                 requestService.saveRequest(request);
 
-               // requestService.saveRequest();
                 redirectAttributes.addFlashAttribute("message", "Demande d'inscription de professeur soumise avec succès !");
             } else {
                 redirectAttributes.addFlashAttribute("errorMessage", "Type d'utilisateur invalide.");
-                return "redirect:/api/"; // Redirige vers la page d'accueil avec un message d'erreur
+                return "redirect:/"; // Redirige vers la page d'accueil avec un message d'erreur
             }
 
         } catch (Exception e) {
             redirectAttributes.addFlashAttribute("errorMessage", "Erreur lors de la soumission de la demande : " + e.getMessage());
-            return "redirect:/api/"; // Redirige vers la page d'accueil avec un message d'erreur
+            return "redirect:/"; // Redirige vers la page d'accueil avec un message d'erreur
         }
 
-        return "redirect:/api/"; // Redirige vers la page d'accueil après une demande réussie
+        return "redirect:/"; // Redirige vers la page d'accueil après une demande réussie
     }
 }
