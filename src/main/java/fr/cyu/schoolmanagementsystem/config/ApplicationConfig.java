9f5ee44b1bde57a1aa1068b09eb9439b70b8ba58
Melvin <melvinhqb@cy-tech.fr>
2024-11-04 15:15:17 +0100

    Create StudentWebController and JSP pages associated

--- src/main/java/fr/cyu/schoolmanagementsystem/config/ApplicationConfig.java
+++ src/main/java/fr/cyu/schoolmanagementsystem/config/ApplicationConfig.java
@@ -3,6 +3,7 @@ package fr.cyu.schoolmanagementsystem.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 
 @Configuration
 public class ApplicationConfig {
@@ -10,6 +11,11 @@ public class ApplicationConfig {
     @Bean
     public ModelMapper createModelMapper(){
         return new ModelMapper();
+    }
+
+    @Bean
+    public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
+        return new HiddenHttpMethodFilter();
     }
 
 }
